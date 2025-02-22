2106587c83c09df6679511f6fb8c3a372b35dbf7
Anthony Fox <anthonydfox04@gmail.com>
2025-02-22 16:27:09 -0500

    youtubeSearch song search function implementation

--- youtubeSearch.py
+++ youtubeSearch.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Sample Python code for youtube.search.list
+# Based on sample Python code for youtube.search.list
 # See instructions for running these code samples locally:
 # https://developers.google.com/explorer-help/code-samples#python
 
@@ -9,23 +9,15 @@ from dotenv import load_dotenv
 
 import os
 
-import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
-# Loads .env variables (API Keys)
-load_dotenv()
-# Gets Youtube API key from .env
-YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+def song_search(song_title):
+    # Loads .env variables (API Keys)
+    load_dotenv()
 
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
-
-song_title = "Blackbird" + "Guitar Tutorial"
-
-def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+    # Gets Youtube API key from .env
+    YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
 
     api_service_name = "youtube"
     api_version = "v3"
@@ -53,7 +45,13 @@ def main():
     )
     response = request.execute()
 
-    print(response)
+    # Organized into a list with a list called items with data fields "snippet", "medium", "high"
+    # Iterates over every element in "items" to find the title, and video id
+    for item in response["items"]:
+        title = item["snippet"]["title"]
+        video_id = item["id"]["videoId"]
 
-if __name__ == "__main__":
-    main()
+    # Video URL Format is "https://www.youtube.com/watch?v=" + video_id
+    video_url = f"https://www.youtube.com/watch?v={video_id}"
+
+    return video_url
